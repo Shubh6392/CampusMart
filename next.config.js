a1d2bd68b486490c9f4c7965df7831ba3921d259
Shubh6392 <shubhsingh6392@gmail.com>
2026-04-30 11:01:46 +0530

    render Images

--- next.config.js
+++ next.config.js
@@ -3,9 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '**.res.cloudinary.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'picsum.photos' },
     ]
   }
 };
